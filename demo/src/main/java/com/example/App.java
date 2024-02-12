e303b9e2494d2529e42e7d7ef22b587f46649442
Brendan Bobryk <babobryk@gmail.com>
2024-02-11 17:43:28 -0800

    added error handling

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -18,9 +18,14 @@ public class App {
             System.out.print("\nEnter a number, or type q to quit: ");
             input = scan.next();
 
-            number = Double.parseDouble(input);
-            newNumber = number + 10.0;
-            System.out.println(number + " + 10 = " + newNumber);
+            try {
+                number = Double.parseDouble(input);
+                newNumber = number + 10.0;
+                System.out.println(number + " + 10 = " + newNumber);
+            } catch (Exception e) {
+                System.out.print("ERROR: " + input + " is not a floating point number. Please try again.\n");
+            }
+
         }
 
         System.out.println("The user quit. Thank you.");
